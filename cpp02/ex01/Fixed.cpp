7503b86f7aad388e610208033d218e9dcaf3c3bf
sakamoto-42 <julien66140@hotmail.fr>
2025-06-13 09:58:16 +0200

    ex01

---
+++ cpp02/ex01/Fixed.cpp
@@ -0,0 +1,94 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.cpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: juduchar <juduchar@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/13 08:18:38 by juduchar          #+#    #+#             */
+/*   Updated: 2025/06/13 09:54:33 by juduchar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <string>
+#include <cmath>
+#include "Fixed.hpp"
+
+const int Fixed::_fractional_bits = 8;
+
+Fixed::Fixed(void) : _fixed_point_value(0)
+{
+	std::cout << "Default constructor called" << std::endl;
+	return ;
+}
+
+Fixed::Fixed(const int integer_number)
+{
+	std::cout << "Int constructor called" << std::endl;
+	this->_fixed_point_value = integer_number << _fractional_bits;
+	return ;
+}
+
+Fixed::Fixed(const float floating_point_number)
+{
+	const float	scale_factor = (float)(1 << _fractional_bits);
+
+	std::cout << "Float constructor called" << std::endl;
+	this->_fixed_point_value = roundf(floating_point_number * scale_factor);
+	return ;
+}
+
+Fixed::Fixed(Fixed const &src)
+{
+	std::cout << "Copy constructor called" << std::endl;
+	*this = src;
+	return ;
+}
+
+Fixed::~Fixed(void)
+{
+	std::cout << "Destructor called" << std::endl;
+	return ;
+}
+
+Fixed	&Fixed::operator=(Fixed const &rhs)
+{
+	std::cout << "Copy assignment operator called" << std::endl;
+	if (this != &rhs)
+	{
+		this->_fixed_point_value = rhs.getRawBits();
+	}
+	return (*this);
+}
+
+int		Fixed::getRawBits(void) const
+{
+	return (this->_fixed_point_value);
+}
+
+void	Fixed::setRawBit(int const raw)
+{
+	std::cout << "setRawBits member function called" << std::endl;
+	this->_fixed_point_value = raw;
+	return ;
+}
+
+float	Fixed::toFloat(void) const
+{
+	const float	scale_factor = (float)(1 << _fractional_bits);
+
+	return (this->_fixed_point_value / scale_factor);
+}
+
+int		Fixed::toInt(void) const
+{
+	
+	return (this->_fixed_point_value >> _fractional_bits);	
+}
+
+std::ostream	&operator<<(std::ostream &o, Fixed const &fixed)
+{
+	o << fixed.toFloat();
+	return (o);
+}
---
+++ cpp02/ex01/Fixed.hpp
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: juduchar <juduchar@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/13 08:02:52 by juduchar          #+#    #+#             */
+/*   Updated: 2025/06/13 09:08:39 by juduchar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FIXED_HPP
+# define FIXED_HPP
+
+# include <iostream>
+
+class	Fixed
+{
+	public:
+		Fixed(void);
+		Fixed(const int integer_number);
+		Fixed(const float floating_point_number);
+		Fixed(Fixed const &src);
+		virtual ~Fixed(void);
+
+		Fixed	&operator=(Fixed const &rhs);
+
+		float	toFloat(void) const;
+		int		toInt(void) const;
+
+		int		getRawBits(void) const;
+		void	setRawBit(int const raw);
+
+	private:
+		int					_fixed_point_value;
+		static const int 	_fractional_bits;	
+};
+
+std::ostream	&operator<<(std::ostream &o, Fixed const &fixed);
+
+#endif
